78a5380438ea2bc0c0aa8bc4085203baab3d9381
Peter Baier <48391460+DK7IH@users.noreply.github.com>
2021-08-11 08:51:19 +0200

    Add files via upload

--- _ReadKey/read_input_key.c
+++ _ReadKey/read_input_key.c
@@ -6,60 +6,40 @@
 /*  Author:           Peter Baier  (DK7IH)                       */
 /*  Last change:      JUL 2021                                   */
 ///////////////////////////////////////////////////////////////////
-//Purpose: This demo reads input pin PE4
+//Purpose: This demo reads input pin PD15 (Board DIYMORE F407VGT6
 
 #include "stm32f4xx.h"
 
-#define BUTTON_PIN 4 //PE4
-
-// Quick and dirty delay
-static void delay (unsigned int time) 
-{
-    for (unsigned int i = 0; i < time; i++)
-    {
-        for (volatile unsigned int j = 0; j < 2000; j++);
-    }    
-}
+#define BTNPIN 15 //PD15
 
 int main (void) 
 {
-
     uint32_t pin_input;
-    //Turn on the GPIOA peripheral (LED out)
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    //Turn on the GPIOE peripheral (Input pin)
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
     
-    //LEDs
-	//Put pin A6 and A7 in general purpose output mode
-    GPIOA->MODER  |=  (1 << (6 << 1));	
-    GPIOA->MODER  |=  (1 << (7 << 1));	
-	
-    //Pin PE4 should be set to 'input' mode with pull-up.
-    GPIOE->MODER  &= ~(3 << (BUTTON_PIN << 1)); //Set to 00 -> Input mode
-    GPIOE->PUPDR  &= ~(3 << (BUTTON_PIN << 1)); //Set to 00	-> No pullup nor pulldown
-    GPIOE->PUPDR  |=  (1 << (BUTTON_PIN << 1)); //Set to 01 -> Pull up
+    //Turn on the GPIOE peripheral for signal LED
+    RCC->AHB1ENR |= (1 << 4);
+    //Put pin PE0 in general purpose output mode
+    GPIOE->MODER |= (1 << (0 << 1));
     
-    //Define output pins B6, B7
-    GPIOA->BSRR = GPIO_BSRR_BS6;
-    GPIOA->BSRR = GPIO_BSRR_BS7;
-
+    //Turn on the GPIOD peripheral (Input pin w. user button)
+    RCC->AHB1ENR |= (1 << 3);
+    
+    //Pin PE4 must be set to 'input' mode with pull-up.
+    GPIOE->MODER  &= ~(3 << (BTNPIN << 1)); //Set to 00b -> Input mode
+    GPIOE->PUPDR  &= ~(3 << (BTNPIN << 1)); //Reset to 00b first 
+    GPIOE->PUPDR  |=  (1 << (BTNPIN << 1)); //Set to 01b -> Pull up
+    
     while(1)
     {
-		pin_input = ~GPIOE->IDR; //"0" means "pressed"!
+		pin_input = ~(GPIOD->IDR); //"0" means "pressed"!
 		if(pin_input & (1 << BUTTON_PIN))
 		{
-            GPIOA->BSRR = GPIO_BSRR_BS7;
+            GPIOE->ODR &= ~(1 << 0);
         }
         else
         { 
-        	
-            GPIOA->BSRR = GPIO_BSRR_BR7;
+        	GPIOE->ODR |= (1 << 0);
         }
-        GPIOA->BSRR = GPIO_BSRR_BS6;
-        delay(100);
-        GPIOA->BSRR = GPIO_BSRR_BR6;
-        delay(100);
     }
 
     return 0;

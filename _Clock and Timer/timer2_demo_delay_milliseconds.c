88c3d717a11ba3056cfe10815d84c16f077c8203
Peter Baier <48391460+DK7IH@users.noreply.github.com>
2021-08-13 10:29:52 +0200

    Add files via upload

--- _Clock and Timer/timer2_demo_delay_milliseconds.c
+++ _Clock and Timer/timer2_demo_delay_milliseconds.c
@@ -1,91 +1,104 @@
 ///////////////////////////////////////////////////////////////////  
-//                    Timer demo for STM32F4                     //
-///////////////////////////////////////////////////////////////////
-//Purpose: This demonstration reads analogue voltage from pin PB0//
-//and displays its numeric value to an LCD DOT matrix module     //
+//                    Clock set for STM32F4 with Timer2 as       //
+//                    counter for seconds                        //  
 ///////////////////////////////////////////////////////////////////
 /*  MCU:              STM32F4 (ARM Cortex M4)                    */
-/*  Hardware:         WeAct Blackpill board                      */
+/*  Hardware:         F411E Discovery board by STM               */
 /*  Compiler:         GCC (GNU ARM TOOLCHAIN)                    */
 /*  Author:           Peter Baier  (DK7IH)                       */
 /*  Last change:      JUL 2021                                   */
 ///////////////////////////////////////////////////////////////////
-//Purpose: This demonstration uses Timer2 to generate a 1kHz signal
-//used for a delay in Milliseconds
-
+//TIM" is configurted to count to 500ms toggling the blue LED so
+//that this produces a 1 second blink
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
-#include <math.h>
+#include <stdlib.h> 
 
   ////////////
  //  MISC  //
 ////////////
 int main(void);
+extern "C" void TIM2_IRQHandler(void);      // IRQ-Handler timer2
+
+  /////////////////////////////
+ //     TIM2 INT Handler    //
+/////////////////////////////
+extern "C" void TIM2_IRQHandler(void)//IRQ-Handler for TIM2
+{
+    if (TIM2->SR & TIM_SR_UIF)       //Toggle LED on update event every 500ms
+    {
+		GPIOD->ODR ^= (1 << 15);     //Blue LED
+    }
+    TIM2->SR = 0x0;                  //Reset status register
+}
 
   ///////////////////
  // Main programm //
 ///////////////////
 int main(void)
 {
-	uint32_t ti = 1000; //Delay time in Milliseconds
-	
-	//////////////////////////////////////////
-    // Setup LED w. GPIOC
-    //////////////////////////////////////////
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; //RCC->AHB1ENR |= (1<<0);
-    GPIOC->MODER |= (1 << (13 << 1));
-	
-	GPIOC->ODR |= (1 << 13);                      //LED off
+    //Turn on the GPIOD peripheral for LED
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; //RCC->AHB1ENR |= (1<<0);
+    GPIOD->MODER |= (1 << (15 << 1));	//LED blue
     
     //////////////////////////////////////////////
-    // Set SystemClock to 50 MHz with 16 MHz HSI
+    // Set SystemClock to 100 MHz with 8 MHz HSE
     //////////////////////////////////////////////
-    FLASH->ACR |= FLASH_ACR_LATENCY_2WS;        //2 wait states
-    RCC->CR |= RCC_CR_HSION;                    //Activate internal clock (HSI: 16 MHz)
-    while ((RCC->CR & RCC_CR_HSIRDY) == 0);     //Wait until HSE is ready
-    RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLQ;
-    RCC->PLLCFGR |= 4 << RCC_PLLCFGR_PLLQ_Pos;  //PLL-Q: /4
-    RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSI;     //PLL source is HSI
-    RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLP;          //PLL-P: /2: f.PLL = f.VCO.out / 2 = 50MHz
-    RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLN;
-    RCC->PLLCFGR |= 50 << RCC_PLLCFGR_PLLN_Pos; //PLL-N: x50: f.VCO.out = 50 x f.VCO.in = 100MHz
-    RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLM;
-    RCC->PLLCFGR |= 8 << RCC_PLLCFGR_PLLM_Pos;  //PLL-M: /8 -> f.VCO.in = f.HSI / 8 = 2 MHZ
-    RCC->CR |= RCC_CR_PLLON;                    //Activate PLL (Output: 50 MHz)
-    while ((RCC->CR & RCC_CR_PLLRDY) == 0);     //Wait until PLL is ready
+    FLASH->ACR |= 0b010;                         //2 wait state for 100 MHz
+    RCC->CR |= (1 << 16);                        //Activate external clock (HSE: 8 MHz)
+    while ((RCC->CR & (1 << 17)) == 0);          //Wait until HSE is ready
     
-    //Division by 2 of clk signal       
-    RCC->CFGR |= RCC_CFGR_HPRE_DIV2             //AHB divider:  /2 =25MHz
-              | RCC_CFGR_PPRE1_DIV2             //APB1 divider: /2 =25MHz
-              | RCC_CFGR_PPRE2_DIV2;            //APB2 divider: /2 =25MHz
-               
-    RCC->CFGR |= RCC_CFGR_SW_PLL;               //Switching to PLL clock source
-
-	//////////////////////////////////////////
-    // Setup TIMER2
-    //////////////////////////////////////////
-    RCC->APB1ENR |= (1 << 0);         //Enable TIM2 clock (Bit0)
- 	TIM2->CR1 = 0;                    //Reset register
-	TIM2->SR = 0;                     //Clear the update event flag
-	TIM2->PSC = 25000;                //Set prescaler to 1kHz
-    TIM2->ARR = 0xFFFF;               //Set delay
-    TIM2->CR1 |= (1 << 0);            //Start the timer counter 
+    //Configuration of PLL
+    RCC->PLLCFGR |= (1 << 22);                  //PLL source is HSE
+    
+                                                //Set PLLM
+    RCC->PLLCFGR &= ~0x3F;                      //1st Reset bits
+    RCC->PLLCFGR |= 4;                          //2nd define VCO input frequency = PLL input clock frequency (f.HSE) / PLLM with 2 ≤ PLLM ≤ 63 
+                                                //-> f.VCO.in = 8MHz / 4 = 2MHz
+                                                
+                                                //Set PLLN: PPLLN defines VCO out frequency
+    RCC->PLLCFGR &= ~0x7FC0;                    //1st Reset bits 14:6
+    RCC->PLLCFGR |= (100 << 6);                 //2nd define f.VCO.out = f.VCO.in * 100 = 200MHz
+     
+                                                //Set PLLP: Main PLL (PLL) division factor for main system clock; Reset Bits 17:16
+    RCC->PLLCFGR &= ~(0b11 << 16);              //Reset bits 17:16
+                                                //f.PLL.output.clock = f.VCO.out / 2 = 100MHz
+                                                
+                                                //Set PLLQ. PLLQ = division factor for USB OTG FS, SDIO and random number generator clocks
+    RCC->PLLCFGR &= ~(0b1111 << 24);            //Reset bits 27:24
+    RCC->PLLCFGR |= (8 << 24);                  //PLL-Q: f.VCO.out / 8 = 25MHz
         
-	GPIOC->ODR &= ~(1 << 13);                      //LED on
-            
-    while(1)
-    { 
-		TIM2->EGR &= ~(1 << 0);            //Timer reset
-        TIM2->CNT = 0x00;                 //Counter register reset
-	    while(TIM2->CNT < ti - 100);
-	    GPIOC->ODR &= ~(1 << 13);         //LED on
-		
-		TIM2->EGR &= ~(1 << 0);            //Timer reset
-        TIM2->CNT = 0x00;                 //Counter register reset
-	    while(TIM2->CNT < 100);
-	    GPIOC->ODR |= (1 << 13);          //LED off
+    RCC->CR |= (1 << 24);                       //Activate PLL, Bit 24
+    while ((RCC->CR & (1 << 25)) == 0);         //Wait until PLL is ready Bit 25
     
-	 
+                                                //Division of clock signal for bus system
+    RCC->CFGR |= (0b1001 << 4)                  //AHB divider:  100MHz / 4 = 25 MHz
+              | (0b100 << 10)                   //APB1 divider: /2
+              | (0b100 << 13);                  //APB2 divider: /2
+               
+    RCC->CFGR |= 0b10;                          //Switching to PLL clock source
+        
+    //Enable TIM2 clock (bit0)
+    RCC->APB1ENR |= (1 << 0);
+    
+    //Timer calculation
+    //Timer update frequency = TIM_CLK/(TIM_PSC+1)/(TIM_ARR + 1) 
+    TIM2->PSC = 100000; //Dive system clock (f=100MHz) by 100000 -> update frequency = 1000/s
+    TIM2->ARR = 500;    //Define overrun after 500ms
+
+    //Update Interrupt Enable
+    TIM2->DIER |= (1 << 0);
+
+    NVIC_SetPriority(TIM2_IRQn, 2); //Priority level 2
+    NVIC_EnableIRQ(TIM2_IRQn);      //Enable TIM2 IRQ from NVIC
+
+    
+    TIM2->CR1 |= (1 << 0);           //Enable Timer 2 module (CEN, bit0)
+
+    while(1)
+    {
     }
+	
 	return 0;
 }
+

e7f02b1481cc2602e42e2e018b966d72f87a3c48
Peter Baier <48391460+DK7IH@users.noreply.github.com>
2021-12-23 18:30:02 +0100

    Add files via upload

--- _USART/usart_tx_rx_interrupt.c
+++ _USART/usart_tx_rx_interrupt.c
@@ -90,7 +90,7 @@ int main(void)
     GPIOA->MODER &= ~(0xFU << 4); //Reset bits 4:5 for PA2 and 6:7 for PA3
     GPIOA->MODER |=  (0xAU << 4); //Set   bits 4:5 for PA2 and 6:7 for PA3 to alternate mode (10)
 
-    //Set pin modes as high speed
+    //Set pin modes as fast speed
     GPIOA->OSPEEDR |= 0xA0 << 4; //Set pin 2/3 to fast speed mode (0b10 << 4)
 
     //Choose AF7 for USART2 in Alternate Function registers

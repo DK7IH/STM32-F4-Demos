8ca1faac657c3d084c5910566cfcac5629356bca
Peter Baier <48391460+DK7IH@users.noreply.github.com>
2022-07-19 00:28:41 +0200

    Add files via upload

--- _Rotary Encoder/rotary_encoder.c
+++ _Rotary Encoder/rotary_encoder.c
@@ -1,93 +1,51 @@
-///////////////////////////////////////////////////////////////////  
-//                    Rotary encoder demo for STM32F4            //
-///////////////////////////////////////////////////////////////////
-/*  MCU:              STM32F4 (ARM Cortex M4)                    */
-/*  Hardware:         WeAct Blackpill board                      */
-/*  Compiler:         GCC (GNU ARM TOOLCHAIN)                    */
-/*  Author:           Peter Baier  (DK7IH)                       */
-/*  Last change:      JUL 2021                                   */
-///////////////////////////////////////////////////////////////////
-//Purpose: This demo uses input pins PB0/PB1 to detect
-//pin change and fire interrupt on EXTI0 when rotary encoder
-//is turned cw/ccw. Rising edge of pin change will be detected.
-//Value of variable "rotate" becomes either 1 or -1 to indicate 
-//direction. This is shown by lighting one of leds (PB14, PB15 to VDD) 
-//installed.
-//
+///////////////////////////////////////////////////////////
+// Demo for rotary encoder (connected to PB0, PB1)       //
+// Uses 2 LEDs to GND with resistor on PB6 and PB7 to    //
+// show rotating direction                               //  
+//                                                       //
+// 2021 by Peter Baier micromaker.de                     //
+///////////////////////////////////////////////////////////
 #include "stm32f4xx.h"
-#include "system_stm32f4xx.h"
-#include <stdlib.h> 
-#include <string.h> 
 
-
-  ////////////
- //  MISC  //
-////////////
+//Interrupt handler for rotary encoder
+extern "C" void EXTI0_IRQHandler(void);
 int main(void);
 
-int16_t rotate = 0;
-int16_t laststate = 0; //Last state of rotary encoder
+int rotate = 0;
 
-static void delay_ms(unsigned int); 
-
-extern "C" void EXTI0_IRQHandler(void);
-
-extern "C" void EXTI0_IRQHandler(void) 
-{ 
-	uint16_t state; 
-		
-	if(EXTI->PR & (1 << 0))  //Check first if the interrupt is triggered by EXTI0 
-	{ 
-		state = GPIOB->IDR & 0x03; //Read pins
+extern "C" void EXTI0_IRQHandler(void)
+{
+   int16_t state; 
+    
+    GPIOC->ODR ^= (1 << 13); //Toggle LED
+    
+    // Check if the interrupt came from exti0
+    if (EXTI->PR & (1 << 0))
+    {
+        state = GPIOB->IDR & 0x03; //Read pins
         if(state & 1)
         {
             if(state & 2)
             {
-                //Turn CW
                 rotate = 1;
             }
             else
             {
-                //Turn CCW
                 rotate = -1;
             }
         }
-        //Clear pending bit
+
+        //Clear bit
         EXTI->PR = (1 << 0);
-    } 
+    }
 }
 
-  /////////////////////////////
- //  Quick and esay delay   //
-/////////////////////////////
-static void delay_ms(unsigned int time) 
-{
-    for (unsigned int i = 0; i < time; i++)
-    {
-        for (volatile unsigned int j = 0; j < 2000; j++);
-    }    
-}
-
-  ///////////////////
- // Main programm //
-///////////////////
 int main(void)
 {
-	int16_t rotate_old = 0;
-	int t1;
-
-    //////////////////////////////////////////
-    // Setup LEDs
-    //////////////////////////////////////////
-	//Turn on the GPIOC peripheral for onboard LED
-    RCC->AHB1ENR |= (1 << 2);           //Enable GPIOC
-	GPIOC->MODER |= (1 << (13 << 1));	//Set PC13 for output
+	//GPIOC power up for LED
+    RCC->AHB1ENR |= (1 << 2);  
+    GPIOC->MODER |= (1 << (13 << 1));                         
 	
-    //Turn on the GPIOB peripheral for LED PB14 and PB15
-    RCC->AHB1ENR |= (1 << 1);            //RCC->AHB1ENR = 0b10
-    GPIOB->MODER |= (1 << (14 << 1));    //Set PB14 for output
-    GPIOB->MODER |= (1 << (15 << 1));    //Set PB15 for output
-    
     //////////////////////////////////////////////
     //Rotary Encoder Setup
     //////////////////////////////////////////////
@@ -101,45 +59,34 @@ int main(void)
     SYSCFG->EXTICR[0] |= 0x0001;  //Write 0b01 to map PB0 to EXTI0
     EXTI->RTSR |= 0x01;           //Enable rising edge trigger on EXTI0
     EXTI->IMR |= 0x01;            //Mask EXTI0
-    
+
     NVIC_SetPriority(EXTI0_IRQn, 1); //Set Priority for each interrupt request Priority level 1
     NVIC_EnableIRQ(EXTI0_IRQn);      //Enable EXT0 IRQ from NVIC
     
-    for(t1 = 0; t1 < 10; t1++)
-    {
-		GPIOB->ODR |= (1 << 14);
-		GPIOB->ODR &= ~(1 << 15);
-		delay_ms(10);
-		GPIOB->ODR &= ~(1 << 14);
-		GPIOB->ODR |= (1 << 15);
-		delay_ms(10);
-	}	
+    //Leds on PB3, PB4 to indicate rotate direction
+  	GPIOB->MODER |= (1 << (6 << 1));  //PB6 as output                        
+    GPIOB->MODER |= (1 << (7 << 1));  //PB7 as output     
     
-    //All LEDs off
-    GPIOC->ODR |= (1 << 13);
-    GPIOB->ODR |= (1 << 14);
-    GPIOB->ODR |= (1 << 15);
+    //GPIOB->ODR &= ~ (1 << 6);
+    //GPIOB->ODR &= ~ (1 << 7);
+        
+    rotate = 0;
     
     while(1)
-    {		
-		if(rotate != rotate_old)
+    {
+		if(rotate == 1)                  
 		{
-		    if(rotate < 0)
-		    {
-				GPIOB->ODR |= (1 << 14);
-		        GPIOB->ODR &= ~(1 << 15);
-		        rotate = 0;
-		    }    
-		    
-		    if(rotate > 0)
-		    {
-		        GPIOB->ODR &= ~(1 << 14);
-		        GPIOB->ODR |= (1 << 15);
-		        rotate = 0;
-		    }
-		    rotate_old = rotate;
-		}   
-    }
-	return 0;
+			GPIOB->ODR &= ~(1 << 6);
+			GPIOB->ODR |= (1 << 7);
+			rotate = 0;
+		}
+		
+		if(rotate == -1)                  
+		{	
+			GPIOB->ODR &= ~(1 << 7);
+		    GPIOB->ODR |= (1 << 6);	
+		    rotate = 0;
+		}	
+	}	
+    
 }
-

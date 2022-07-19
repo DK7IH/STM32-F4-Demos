a7ceb91c40cd7d0fe223d599bb757040ad947e57
Peter Baier <48391460+DK7IH@users.noreply.github.com>
2022-07-19 10:33:36 +0200

    Add files via upload

--- _Clock and Timer/clockset_168MHz_w_25MHz_HSE_w_MCO.c
+++ _Clock and Timer/clockset_168MHz_w_25MHz_HSE_w_MCO.c
@@ -46,21 +46,23 @@ int main(void)
     GPIOE->ODR |= (1 << 0);             //Reset data register out
  
     //////////////////////////////////////////
-    // Prepare MCO at PA8
+    // Prepare MCO at PA8 
     //////////////////////////////////////////
     //Turn on GPIOA
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; //RCC->AHB1ENR |= (1<<0);
     
     //Set PA8 as MCO pin
     RCC->CFGR |= (0b11 << 21);          //MCO1: Microcontroller clock output 1 11: PLL clock selected
-    //RCC->CFGR |= (0b100 <<24);          //Divide f.out by 2 uncomment this line if your counter/scope is not for 100MHz!
+    //RCC->CFGR |= (0b100 <<24);          //Divide f.out by 2 -> uncomment this line if your counter/scope is not for 100MHz!
     GPIOA->MODER |= (2 << (8 << 1));	//PA8 as AF
     GPIOA->OSPEEDR |= (3 << (8 << 1));	//HiSpeed
     GPIOA->AFR[1] = 0;                  //0b0000   
+    //This section is for verification of the clock frequency by means
+    //of a frequency counter, uncomment when not used!
     
-    //////////////////////////////////////////////
-    // Set SystemClock to 168 MHz with 25 MHz HSE
-    //////////////////////////////////////////////
+    ///////////////////////////////////////////////////
+    // Set SystemClock to 168.75 MHz with 25 MHz HSE //
+    ///////////////////////////////////////////////////
     FLASH->ACR |= 0b010;                         //2 wait state for 100 MHz
     RCC->CR |= (1 << 16);                        //Activate external clock (HSE: 8 MHz)
     while ((RCC->CR & (1 << 17)) == 0);          //Wait until HSE is ready
@@ -70,16 +72,16 @@ int main(void)
     
                                                 //Set PLLM
     RCC->PLLCFGR &= ~0x3F;                      //1st Reset bits
-    RCC->PLLCFGR |= 10;                          //2nd define VCO input frequency = PLL input clock frequency (f.HSE) / PLLM with 2 ≤ PLLM ≤ 63 
-                                                //-> f.VCO.in = 25MHz / 10 = 2.5MHz
+    RCC->PLLCFGR |= 14;                          //2nd define VCO input frequency = PLL input clock frequency (f.HSE) / PLLM with 2 ≤ PLLM ≤ 63 
+                                                //-> f.VCO.in = 25MHz / 20 = 1.25MHz
                                                 
                                                 //Set PLLN: PPLLN defines VCO out frequency
     RCC->PLLCFGR &= ~0x7FC0;                    //1st Reset bits 14:6
-    RCC->PLLCFGR |= (160 << 6);                 //2nd define f.VCO.out = f.VCO.in * 160 = 400MHz
+    RCC->PLLCFGR |= (189 << 6);                 //2nd define f.VCO.out = f.VCO.in * 320 = 400MHz
      
                                                 //Set PLLP: Main PLL (PLL) division factor for main system clock; Reset Bits 17:16
     RCC->PLLCFGR &= ~(0b11 << 16);              //Reset bits 17:16
-                                                //f.PLL.output.clock = f.VCO.out / 2 = 200MHz
+    RCC->PLLCFGR |= (0b00 << 16);               //f.PLL.output.clock = f.VCO.out / 2 = 200MHz
                                                 
                                                 //Set PLLQ. PLLQ = division factor for USB OTG FS, SDIO and random number generator clocks
     RCC->PLLCFGR &= ~(0b1111 << 24);            //Reset bits 27:24
